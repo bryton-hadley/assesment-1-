0e9abae3af2ffc5f558e0bf28cd0fd4c596d5d1f
Bryton Haldey <bryton.hadley@gmail.com>
2022-08-20 13:47:45 -0600

    deffintions final commit

--- sdlc.js
+++ sdlc.js
@@ -23,12 +23,12 @@ then i will be setting the  varibles equal to there defintions by using ""*/
 
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 
-const analysisOfRequirements = " This is the second step, in which the software reguirements are"
+const analysisOfRequirements = " This is the second step, this step is wear you check all you steps and help you plan what you need "
 
-const design = ""
+const design = " this is the third step, in which the end goal is to finalize your plan and how your website will work before you put fingers on keboards  "
 
-const implementation = ""
+const implementation = " this is the fourth step, in which the in goal is to start writing code and building your project "
 
-const testingIntegration = ""
+const testingIntegration = " this is the fith step, in which the goal is to test your code and deployed "
 
-const maintain = ""
+const maintain = "this is the sixth step, in which there are things that didnt get covered in your plan or new fetures you want to put into the site"
--- variables.js
+++ variables.js
@@ -52,7 +52,6 @@ if( faveNum === 13){
 /* i am going to set this up with an for() then I will set i to eqaul my faveNums then i would set i less then 5 because we want it to run 5 times
 then i woul use i++ to making it add i 5 times. to make this all work i will console.log(i) this will make " faveNum" pop up 5 times*/
 for(let i = faveNum; i <= 13; i++){
-    
-  console.log(i)
+    console.log(i)
 
    }
