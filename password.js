47b2d93aa34c55e3fd8ee557cccc2f96c7e15f4d
Bryton Haldey <bryton.hadley@gmail.com>
2022-08-21 15:46:01 -0600

    password checking for number

--- password.js
+++ password.js
@@ -1,17 +1,26 @@
+// password check to see if the password is true or false 
+
 let password = 'Pirate1331'
 
-// checks varibles to make sure requirements are met 
+// Declares varibles to make sure requirements are met 
 
 let minChar = false
 let upperCase = false
+let numCheck = false
 
+// I will use a for loop to check the requirments are met for numcheck by using for()
 
 for(let i = 0; i < password.length; i++){
-    console.log(password[i])
+    console.log(password[i] * 1)
+    if(password[i] !== NaN){
+        numCheck = true
+    }
+    if(password[i] === )
 }
-//check that password meets minimum characters 
-if(password >= 10){
-minChar = true
-} else if()
 
-console.log(minChars)
+//check that password meets minimum characters 
+if(password.length >= 10){
+minChar = true
+} 
+
+console.log(minChar)
